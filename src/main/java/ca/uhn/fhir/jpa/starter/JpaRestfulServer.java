8d0d30d3147c162ad8bebf5829928536928ae5e4
Joris Scharp <jorisscharp@gmail.com>
2021-06-30 09:09:11 +0200

    Allowing users to set the meta.source with the `X-Request-Source` header.

--- src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
@@ -3,6 +3,7 @@ package ca.uhn.fhir.jpa.starter;
 import ca.uhn.fhir.jpa.starter.koppeltaal.config.FhirServerSecurityConfiguration;
 import ca.uhn.fhir.jpa.starter.koppeltaal.interceptor.JwtSecurityInterceptor;
 import ca.uhn.fhir.jpa.starter.koppeltaal.interceptor.Oauth2UrisStatementInterceptorForR4;
+import ca.uhn.fhir.rest.server.interceptor.CaptureResourceSourceFromHeaderInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 
@@ -33,6 +34,9 @@ public class JpaRestfulServer extends BaseJpaRestfulServer {
 	  }
 
 	  registerInterceptor(new Oauth2UrisStatementInterceptorForR4(fhirServerSecurityConfiguration));
+
+	  //  Allow users to set the meta.source with the "X-Request-Source" header
+	  registerInterceptor(new CaptureResourceSourceFromHeaderInterceptor(getFhirContext()));
 
 	  // Register our custom  structured definitions
 //	  interceptorService.registerInterceptor(factory.build());
