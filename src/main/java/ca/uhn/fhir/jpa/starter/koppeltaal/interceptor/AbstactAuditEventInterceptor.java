dc05e8d95b2edf91f57aa33e41ef0d6050455332
Roland Groen <roland@edia.nl>
2022-07-08 16:20:34 +0200

    KOP-280, Logging werkt niet. De KT Resource server zou automatisch logging aan moeten maken. Dit gebeurt niet. (Bevinding 28)
     - Fixing logging, added bundled events.

--- src/main/java/ca/uhn/fhir/jpa/starter/koppeltaal/interceptor/AbstactAuditEventInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/koppeltaal/interceptor/AbstactAuditEventInterceptor.java
@@ -10,119 +10,148 @@ import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.server.servlet.ServletRequestDetails;
 import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.instance.model.api.IBaseResource;
-import org.hl7.fhir.r4.model.AuditEvent;
-import org.hl7.fhir.r4.model.Device;
-import org.hl7.fhir.r4.model.Resource;
+import org.hl7.fhir.r4.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 import java.util.UUID;
 
 /**
  *
  */
 public abstract class AbstactAuditEventInterceptor {
-	private static final Logger LOG = LoggerFactory.getLogger(AbstactAuditEventInterceptor.class);
-	protected final AuditEventService auditEventService;
-	protected final IFhirResourceDao<Device> deviceDao;
-	protected final DaoRegistry daoRegistry;
+  private static final Logger LOG = LoggerFactory.getLogger(AbstactAuditEventInterceptor.class);
+  protected final AuditEventService auditEventService;
+  protected final IFhirResourceDao<Device> deviceDao;
+  protected final DaoRegistry daoRegistry;
 
-	public AbstactAuditEventInterceptor(AuditEventService auditEventService, DaoRegistry daoRegistry) {
-		this.auditEventService = auditEventService;
-		this.deviceDao = daoRegistry.getResourceDao(Device.class);
-		this.daoRegistry = daoRegistry;
-	}
+  public AbstactAuditEventInterceptor(AuditEventService auditEventService, DaoRegistry daoRegistry) {
+    this.auditEventService = auditEventService;
+    this.deviceDao = daoRegistry.getResourceDao(Device.class);
+    this.daoRegistry = daoRegistry;
+  }
 
-	private void addResources(RequestDetails requestDetails, AuditEventDto dto, IBaseResource... resources) {
-		for (IBaseResource resource : resources) {
-			if (resource instanceof Resource) {
-				dto.addResource((Resource) resource);
-			}
-		}
-	}
+  private void addResources(AuditEventDto dto, IBaseResource... resources) {
+    for (IBaseResource resource : resources) {
+      if (resource instanceof Resource) {
+        dto.addResource((Resource) resource);
+      }
+    }
+  }
 
-	private Device buildDevice(RequestDetails requestDetails) {
-		return ResourceOriginUtil.getDevice(requestDetails, deviceDao).orElse(null);
-	}
+  private Device buildDevice(RequestDetails requestDetails) {
+    return ResourceOriginUtil.getDevice(requestDetails, deviceDao).orElse(null);
+  }
 
-	protected void ensureTransactionGuid(RequestDetails requestDetails) {
-		if (StringUtils.isBlank(requestDetails.getTransactionGuid())) {
-			requestDetails.setTransactionGuid(UUID.randomUUID().toString());
-		}
-	}
+  protected void ensureTransactionGuid(RequestDetails requestDetails) {
+    if (StringUtils.isBlank(requestDetails.getTransactionGuid())) {
+      requestDetails.setTransactionGuid(UUID.randomUUID().toString());
+    }
+  }
 
-	private String getResourceQuery(ServletRequestDetails requestDetails) {
-		String completeUrl = requestDetails.getCompleteUrl();
-		return StringUtils.removeStart(completeUrl, requestDetails.getFhirServerBase());
-	}
+  private String getResourceQuery(ServletRequestDetails requestDetails) {
+    String completeUrl = requestDetails.getCompleteUrl();
+    return StringUtils.removeStart(completeUrl, requestDetails.getFhirServerBase());
+  }
 
-	protected void setDevice(RequestDetails requestDetails, AuditEventDto dto) {
-		dto.setDevice(buildDevice(requestDetails));
-	}
+  protected void setDevice(RequestDetails requestDetails, AuditEventDto dto) {
+    dto.setDevice(buildDevice(requestDetails));
+  }
 
-	protected void setInteraction(ServletRequestDetails requestDetails, AuditEventDto dto) {
-		dto.setSpanId(requestDetails.getRequestId());
-		dto.setTraceId(requestDetails.getTransactionGuid());
-		dto.setParentSpanId((String) requestDetails.getUserData().get("parentSpanId"));
-	}
+  protected void setInteraction(ServletRequestDetails requestDetails, AuditEventDto dto) {
+    dto.setSpanId(requestDetails.getRequestId());
+    dto.setTraceId(requestDetails.getTransactionGuid());
+    dto.setParentSpanId((String) requestDetails.getUserData().get("parentSpanId"));
+  }
 
-	protected boolean setRequestType(ServletRequestDetails requestDetails, AuditEventDto dto) {
-		RestOperationTypeEnum restOperationType = requestDetails.getRestOperationType();
-		String method = requestDetails.getServletRequest().getMethod();
-		if (restOperationType != null) {
-			switch (restOperationType) {
-				case CREATE:
-					dto.setEventType(AuditEventDto.EventType.Create);
-					return true;
-				case READ:
-					dto.setEventType(AuditEventDto.EventType.Read);
-					return true;
-				case SEARCH_TYPE:
-					dto.setEventType(AuditEventDto.EventType.Search);
-					return true;
-				case UPDATE:
-					dto.setEventType(AuditEventDto.EventType.Update);
-					return true;
-				case DELETE:
-					dto.setEventType(AuditEventDto.EventType.Delete);
-					return true;
-				case METADATA:
-					dto.setEventType(AuditEventDto.EventType.Capability);
-					return true;
-				default:
-					LOG.warn(String.format("Unhandled / unknown event type: %s", restOperationType));
-			}
-		} else if (StringUtils.isNotEmpty(method)) {
-			switch (method) {
-				case "POST":
-					dto.setEventType(AuditEventDto.EventType.Create);
-					return true;
-				case "GET":
-					dto.setEventType(AuditEventDto.EventType.Read); // Or Search?
-					return true;
-				case "PUT":
-					dto.setEventType(AuditEventDto.EventType.Update);
-					return true;
-				case "DELETE":
-					dto.setEventType(AuditEventDto.EventType.Delete);
-					return true;
+  protected boolean setRequestType(ServletRequestDetails requestDetails, AuditEventDto dto) {
+    RestOperationTypeEnum restOperationType = requestDetails.getRestOperationType();
+    String method = requestDetails.getServletRequest().getMethod();
+    if (restOperationType != null) {
+      switch (restOperationType) {
+        case CREATE:
+          dto.setEventType(AuditEventDto.EventType.Create);
+          return true;
+        case READ:
+          dto.setEventType(AuditEventDto.EventType.Read);
+          return true;
+        case SEARCH_TYPE:
+          dto.setEventType(AuditEventDto.EventType.Search);
+          return true;
+        case UPDATE:
+          dto.setEventType(AuditEventDto.EventType.Update);
+          return true;
+        case DELETE:
+          dto.setEventType(AuditEventDto.EventType.Delete);
+          return true;
+        case METADATA:
+          dto.setEventType(AuditEventDto.EventType.Capability);
+          return true;
+        default:
+          LOG.warn(String.format("Unhandled / unknown event type: %s", restOperationType));
+      }
+    } else if (StringUtils.isNotEmpty(method)) {
+      switch (method) {
+        case "POST":
+          dto.setEventType(AuditEventDto.EventType.Create);
+          return true;
+        case "GET":
+          dto.setEventType(AuditEventDto.EventType.Read); // Or Search?
+          return true;
+        case "PUT":
+          dto.setEventType(AuditEventDto.EventType.Update);
+          return true;
+        case "DELETE":
+          dto.setEventType(AuditEventDto.EventType.Delete);
+          return true;
 
-			}
-		}
-		return false;
-	}
+      }
+    }
+    return false;
+  }
 
-	protected void storageEvent(ServletRequestDetails requestDetails, IBaseResource resource) {
-		if (!(resource instanceof AuditEvent)) {
-			AuditEventDto dto = new AuditEventDto();
-			if (setRequestType(requestDetails, dto)) {
-				setInteraction(requestDetails, dto);
-				setDevice(requestDetails, dto);
-				addResources(requestDetails, dto, resource);
-				dto.setQuery(getResourceQuery(requestDetails));
-				dto.setOutcome("0");
-				auditEventService.submitAuditEvent(dto, requestDetails);
-			}
-		}
-	}
+  protected void storageEvent(ServletRequestDetails requestDetails, IBaseResource resource) {
+    if (!(resource instanceof AuditEvent)) {
+      AuditEventDto dto = new AuditEventDto();
+      if (setRequestType(requestDetails, dto)) {
+        setInteraction(requestDetails, dto);
+        setDevice(requestDetails, dto);
+        addResources(dto, resource);
+        if (resource instanceof Bundle) {
+          Bundle bundle = (Bundle) resource;
+          List<Bundle.BundleEntryComponent> entry = bundle.getEntry();
+          if (entry != null) {
+            for (Bundle.BundleEntryComponent bundleEntryComponent : entry) {
+              addResources(dto, bundleEntryComponent.getResource());
+            }
+
+          }
+        }
+        dto.setQuery(getResourceQuery(requestDetails));
+        dto.setOutcome("0");
+        auditEventService.submitAuditEvent(dto, requestDetails);
+      }
+    }
+  }
+
+  private void storeActionKey(HttpServletRequest servletRequest, String actionKey) {
+    if (StringUtils.isNotEmpty(actionKey)) {
+      servletRequest.setAttribute(actionKey, true);
+    }
+  }
+
+  private boolean hasActionKey(HttpServletRequest servletRequest, String actionKey) {
+    return StringUtils.isNotEmpty(actionKey) && servletRequest.getAttribute(actionKey) != null;
+  }
+
+  private String getActionKey(AuditEventDto dto) {
+    AuditEventDto.EventType eventType = dto.getEventType();
+    if (dto.getResources().size() == 1) {
+      Resource resource = dto.getResources().get(0);
+      return eventType.name() + "/" + resource.getId();
+    }
+    return null;
+  }
 }
--- src/main/java/ca/uhn/fhir/jpa/starter/koppeltaal/interceptor/AuditEventInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/koppeltaal/interceptor/AuditEventInterceptor.java
@@ -27,12 +27,12 @@ import java.util.UUID;
 @Component
 @Interceptor
 public class AuditEventInterceptor extends AbstactAuditEventInterceptor {
-	private static final Logger LOG = LoggerFactory.getLogger(AuditEventInterceptor.class);
+  private static final Logger LOG = LoggerFactory.getLogger(AuditEventInterceptor.class);
 
-	public AuditEventInterceptor(DaoRegistry daoRegistry, AuditEventService auditEventService) {
-		super(auditEventService, daoRegistry);
+  public AuditEventInterceptor(DaoRegistry daoRegistry, AuditEventService auditEventService) {
+    super(auditEventService, daoRegistry);
 
-	}
+  }
 
 //	@Hook(Pointcut.SERVER_INCOMING_REQUEST_PRE_HANDLED)
 //	public void start(ServletRequestDetails requestDetails) {
@@ -44,100 +44,101 @@ public class AuditEventInterceptor extends AbstactAuditEventInterceptor {
 //		}
 //	}
 
-	@Hook(Pointcut.STORAGE_PRECOMMIT_RESOURCE_CREATED)
-	public void creating(ServletRequestDetails requestDetails, IBaseResource resource) {
-		storageEvent(requestDetails, resource);
-	}
+  @Hook(Pointcut.STORAGE_PRECOMMIT_RESOURCE_CREATED)
+  public void creating(ServletRequestDetails requestDetails, IBaseResource resource) {
+    storageEvent(requestDetails, resource);
+  }
 
-	@Hook(Pointcut.STORAGE_PRECOMMIT_RESOURCE_DELETED)
-	public void deleting(ServletRequestDetails requestDetails, IBaseResource resource) {
-		storageEvent(requestDetails, resource);
-	}
+  @Hook(Pointcut.STORAGE_PRECOMMIT_RESOURCE_DELETED)
+  public void deleting(ServletRequestDetails requestDetails, IBaseResource resource) {
+    storageEvent(requestDetails, resource);
+  }
 
-	@Hook(Pointcut.SERVER_OUTGOING_FAILURE_OPERATIONOUTCOME)
-	public void outgoingException(ServletRequestDetails requestDetails, IBaseOperationOutcome operationOutcome) {
-		AuditEventDto dto = new AuditEventDto();
-		if (setRequestType(requestDetails, dto)) {
-			setInteraction(requestDetails, dto);
-			setDevice(requestDetails, dto);
-			setOutcome(operationOutcome, dto);
-			auditEventService.submitAuditEvent(dto, requestDetails);
-		}
-	}
+  @Hook(Pointcut.SERVER_OUTGOING_FAILURE_OPERATIONOUTCOME)
+  public void outgoingException(ServletRequestDetails requestDetails, IBaseOperationOutcome operationOutcome) {
+    AuditEventDto dto = new AuditEventDto();
+    if (setRequestType(requestDetails, dto)) {
+      setInteraction(requestDetails, dto);
+      setDevice(requestDetails, dto);
+      setOutcome(operationOutcome, dto);
+      auditEventService.submitAuditEvent(dto, requestDetails);
+    }
+  }
 
-	@Hook(Pointcut.SERVER_OUTGOING_RESPONSE)
-	public void outgoingResponse(ServletRequestDetails requestDetails, IBaseResource resource, ResponseDetails responseDetails) {
-		if (!StringUtils.equals("AuditEvent", requestDetails.getResourceName())) {
-			storageEvent(requestDetails, resource);
-		}
-	}
+  @Hook(Pointcut.SERVER_OUTGOING_RESPONSE)
+  public void outgoingResponse(ServletRequestDetails requestDetails, IBaseResource resource, ResponseDetails responseDetails) {
+    if (resource.getIdElement() != null
+      && resource.getIdElement().getValue() != null) {
+      storageEvent(requestDetails, resource);
+    }
+  }
 
-	@Hook(Pointcut.SERVER_INCOMING_REQUEST_PRE_HANDLED)
-	public void start(ServletRequestDetails requestDetails) {
+  @Hook(Pointcut.SERVER_INCOMING_REQUEST_PRE_HANDLED)
+  public void start(ServletRequestDetails requestDetails) {
 
-		// traceId > transaction id
-		// spanId > request id
-		// parentSpanId > userdata['parentSpanId']
+    // traceId > transaction id
+    // spanId > request id
+    // parentSpanId > userdata['parentSpanId']
 
-		HttpServletRequest servletRequest = requestDetails.getServletRequest();
-		String traceId = servletRequest.getHeader("X-B3-TraceId");
-		String spanId = servletRequest.getHeader("X-B3-SpanId");
-		String parentSpanId = servletRequest.getHeader("X-B3-ParentSpanId");
+    HttpServletRequest servletRequest = requestDetails.getServletRequest();
+    String traceId = servletRequest.getHeader("X-B3-TraceId");
+    String spanId = servletRequest.getHeader("X-B3-SpanId");
+    String parentSpanId = servletRequest.getHeader("X-B3-ParentSpanId");
 
-		// Set the requestId if not set. It is the spanId of this request.
-		if (StringUtils.isBlank(requestDetails.getRequestId())) {
-			requestDetails.setRequestId(UUID.randomUUID().toString());
-		}
+    // Set the requestId if not set. It is the spanId of this request.
+    if (StringUtils.isBlank(requestDetails.getRequestId())) {
+      requestDetails.setRequestId(UUID.randomUUID().toString());
+    }
 
-		// Set the traceId, generate one if none exists.
-		if (StringUtils.isNotEmpty(traceId)) {
-			requestDetails.setTransactionGuid(traceId);
-		} else {
-			requestDetails.setTransactionGuid(UUID.randomUUID().toString());
-		}
+    // Set the traceId, generate one if none exists.
+    if (StringUtils.isNotEmpty(traceId)) {
+      requestDetails.setTransactionGuid(traceId);
+    } else {
+      requestDetails.setTransactionGuid(UUID.randomUUID().toString());
+    }
 
-		// Move the incoming span id to the parentSpanId
-		requestDetails.getUserData().put("parentSpanId", spanId);
+    // Move the incoming span id to the parentSpanId
+    requestDetails.getUserData().put("parentSpanId", spanId);
 
-		LOG.info(String.format("Incoming request, traceId='%s', spanId='%s', parentSpanId='%s'", requestDetails.getTransactionGuid(), requestDetails.getRequestId(), parentSpanId));
+    LOG.info(String.format("Incoming request, traceId='%s', spanId='%s', parentSpanId='%s'", requestDetails.getTransactionGuid(), requestDetails.getRequestId(), parentSpanId));
 
 
-	}
+  }
 
-	@Hook(Pointcut.STORAGE_PRECOMMIT_RESOURCE_UPDATED)
-	public void updating(ServletRequestDetails requestDetails, IBaseResource resource) {
-		storageEvent(requestDetails, resource);
-	}
+  @Hook(Pointcut.STORAGE_PRECOMMIT_RESOURCE_UPDATED)
+  public void updating(ServletRequestDetails requestDetails, IBaseResource resource) {
+    storageEvent(requestDetails, resource);
+  }
 
-	private String getTransactionGuid(RequestDetails requestDetails) {
-		String transactionGuid = requestDetails.getTransactionGuid();
-		if (StringUtils.isEmpty(transactionGuid)) {
-			transactionGuid = UUID.randomUUID().toString();
-			requestDetails.setTransactionGuid(transactionGuid);
-		}
-		return transactionGuid;
-	}
+  private String getTransactionGuid(RequestDetails requestDetails) {
+    String transactionGuid = requestDetails.getTransactionGuid();
+    if (StringUtils.isEmpty(transactionGuid)) {
+      transactionGuid = UUID.randomUUID().toString();
+      requestDetails.setTransactionGuid(transactionGuid);
+    }
+    return transactionGuid;
+  }
 
-	private void setOutcome(IBaseOperationOutcome operationOutcome, AuditEventDto dto) {
-		if (operationOutcome instanceof OperationOutcome) {
-			List<OperationOutcome.OperationOutcomeIssueComponent> issue = ((OperationOutcome) operationOutcome).getIssue();
-			if (!issue.isEmpty()) {
-				OperationOutcome.OperationOutcomeIssueComponent outcome = issue.get(0);
-				OperationOutcome.IssueSeverity severity = outcome.getSeverity();
-				switch (severity) {
-					case WARNING:
-					case ERROR:
-						dto.setOutcome("4");
-						break;
-					case INFORMATION:
-					case NULL:
-					default:
-						dto.setOutcome("0");
-						break;
+  private void setOutcome(IBaseOperationOutcome operationOutcome, AuditEventDto dto) {
+    if (operationOutcome instanceof OperationOutcome) {
+      List<OperationOutcome.OperationOutcomeIssueComponent> issue = ((OperationOutcome) operationOutcome).getIssue();
+      if (!issue.isEmpty()) {
+        OperationOutcome.OperationOutcomeIssueComponent outcome = issue.get(0);
+        OperationOutcome.IssueSeverity severity = outcome.getSeverity();
+        switch (severity) {
+          case WARNING:
+          case ERROR:
+            dto.setOutcome("4");
+            break;
+          case INFORMATION:
+          case NULL:
+          default:
+            dto.setOutcome("0");
+            break;
 
-				}
-			}
-		}
-	}
+        }
+      }
+    }
+  }
 
 }
