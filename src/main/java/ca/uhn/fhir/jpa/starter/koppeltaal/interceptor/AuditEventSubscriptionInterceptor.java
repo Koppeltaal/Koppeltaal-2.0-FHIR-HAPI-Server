e2e05a7f859cf2f84318f46b44ca8d7617b5311b
Joris Scharp <joris@headease.nl>
2022-07-22 12:04:21 +0200

    Properly providing the traceId on the subscription request. Unfortunately, there is no access to the request-id in the subscription pointcuts. Added a todo as you'd need to provide this as an X-Correlation-Id header.

--- src/main/java/ca/uhn/fhir/jpa/starter/koppeltaal/interceptor/AuditEventSubscriptionInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/koppeltaal/interceptor/AuditEventSubscriptionInterceptor.java
@@ -5,22 +5,20 @@ import ca.uhn.fhir.interceptor.api.Hook;
 import ca.uhn.fhir.interceptor.api.Interceptor;
 import ca.uhn.fhir.interceptor.api.Pointcut;
 import ca.uhn.fhir.jpa.api.dao.DaoRegistry;
-import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
 import ca.uhn.fhir.jpa.starter.koppeltaal.dto.AuditEventDto;
 import ca.uhn.fhir.jpa.starter.koppeltaal.service.AuditEventService;
 import ca.uhn.fhir.jpa.subscription.model.CanonicalSubscription;
 import ca.uhn.fhir.jpa.subscription.model.ResourceDeliveryMessage;
-import ca.uhn.fhir.jpa.subscription.model.ResourceModifiedMessage;
-import org.hl7.fhir.instance.model.api.IBaseResource;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.hl7.fhir.r4.model.AuditEvent;
-import org.hl7.fhir.r4.model.Device;
 import org.hl7.fhir.r4.model.Resource;
+import org.hl7.fhir.r4.model.StringType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
-import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -38,13 +36,20 @@ public class AuditEventSubscriptionInterceptor extends AbstactAuditEventIntercep
 
 	@Hook(value = Pointcut.SUBSCRIPTION_BEFORE_DELIVERY, order = Integer.MAX_VALUE)
 	public void outgoingSubscription(CanonicalSubscription canonicalSubscription, ResourceDeliveryMessage message) {
-		String transactionId = message.getTransactionId();
-		Resource resource = (Resource)message.getPayload(fhirContext);
-		if (!(resource instanceof AuditEvent)) {
+		String traceId = message.getTransactionId();
+    String requestId = RandomStringUtils.randomAlphanumeric(16); //async, so always generate a new requestId
+
+    //TODO: Ideally, the request-id provided by the request that triggered this subscription is passed down as a X-Correlation-Id header. However, the requestId seems inaccessible (see ca.uhn.fhir.rest.server.messaging.BaseResourceModifiedMessage.BaseResourceModifiedMessage(ca.uhn.fhir.context.FhirContext, org.hl7.fhir.instance.model.api.IBaseResource, ca.uhn.fhir.rest.server.messaging.BaseResourceMessage.OperationTypeEnum, ca.uhn.fhir.rest.api.server.RequestDetails)
+
+    Resource resource = (Resource)message.getPayload(fhirContext);
+
+    setTraceAndRequestIdHeaders(canonicalSubscription, traceId, requestId);
+
+    if (!(resource instanceof AuditEvent)) {
 			AuditEventDto dto = new AuditEventDto();
 			dto.setEventType(AuditEventDto.EventType.SendNotification);
-			dto.setTraceId(transactionId);
-			dto.setSpanId(message.getPayloadId());
+			dto.setTraceId(traceId);
+			dto.setSpanId(requestId);
 			dto.setOutcome("0");
 			dto.addResource(resource);
 			dto.setQuery(canonicalSubscription.getCriteriaString());
@@ -53,6 +58,20 @@ public class AuditEventSubscriptionInterceptor extends AbstactAuditEventIntercep
 		}
 
 	}
+
+  private void setTraceAndRequestIdHeaders(CanonicalSubscription canonicalSubscription, String transactionId, String requestId) {
+    //There is no access to the response headers on the HttpServletResponse for subscriptions. Adding them in-memory to the subscription headers.
+    //Cleaning up to make sure the previous in-memory results aren't sent as well
+    canonicalSubscription.setHeaders(
+      canonicalSubscription.getHeaders().stream()
+        .filter(header -> !header.startsWith("X-Trace-Id: ") && !header.startsWith("X-Request-Id: "))
+        .map(StringType::new)
+        .collect(Collectors.toList())
+    );
+
+    canonicalSubscription.addHeader("X-Trace-Id: " + transactionId);
+    canonicalSubscription.addHeader("X-Request-Id: " + requestId);
+  }
 
 
 }
