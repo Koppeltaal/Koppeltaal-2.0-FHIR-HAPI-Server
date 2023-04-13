36f2868a21ab101ced13185473566af00227d530
Roland Groen <roland@edia.nl>
2023-04-13 13:24:27 +0200

    KOP-559
     - Placed comment to what goes wrong.

--- src/main/java/ca/uhn/fhir/jpa/starter/koppeltaal/service/JwtValidationService.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/koppeltaal/service/JwtValidationService.java
@@ -64,7 +64,7 @@ public class JwtValidationService {
 		DecodedJWT decode = JWT.decode(token);
 		String algorithmName = decode.getAlgorithm();
 		// Lookup the issuer.
-    // TODO: Now the issuer is just fetched as JWKS URL, need to whitelist.
+    // TODO: KOP-559 Now the issuer is just fetched as JWKS URL, need to whitelist.
 		String issuer = decode.getIssuer();
 
 		JwkProvider provider = new UrlJwkProvider(issuer);
