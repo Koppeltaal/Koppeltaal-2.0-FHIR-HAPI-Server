d54dfb8861b0db890a714e911c47231547cfd35c
Joris Scharp <joris@headease.nl>
2023-06-20 16:16:49 +0200

    KOP-374: Fixed PackageInstallerWithExclusionSvcImpl, added custom logic to newest version of the PackageInstallerSvcImpl (v6.6.0 tag) and did the same for the DaoResourceLinkResolverOverride

--- src/main/java/ca/uhn/fhir/jpa/starter/koppeltaal/bean/DaoResourceLinkResolverOverride.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/koppeltaal/bean/DaoResourceLinkResolverOverride.java
@@ -1,20 +1,17 @@
 package ca.uhn.fhir.jpa.starter.koppeltaal.bean;
 
-import ca.uhn.fhir.context.RuntimeSearchParam;
 import ca.uhn.fhir.interceptor.model.RequestPartitionId;
 import ca.uhn.fhir.jpa.dao.index.DaoResourceLinkResolver;
 import ca.uhn.fhir.jpa.model.cross.IResourceLookup;
+import ca.uhn.fhir.jpa.searchparam.extractor.PathAndRef;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.api.server.storage.TransactionDetails;
-import org.hl7.fhir.instance.model.api.IBaseReference;
-import org.hl7.fhir.instance.model.api.IBaseResource;
-import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.r4.model.Bundle;
-import org.hl7.fhir.r4.model.CapabilityStatement;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Nonnull;
 
 /**
  * Class that overrides {@link DaoResourceLinkResolver} only to exclude Bundles from the referential integrity check.
@@ -27,13 +24,13 @@ public class DaoResourceLinkResolverOverride extends DaoResourceLinkResolver {
   private static final Logger LOG = LoggerFactory.getLogger(DaoResourceLinkResolverOverride.class);
 
   @Override
-  public IResourceLookup findTargetResource(@NotNull RequestPartitionId theRequestPartitionId, RuntimeSearchParam theSearchParam, String theSourcePath, IIdType theSourceResourceId, String theResourceType, Class<? extends IBaseResource> theType, IBaseReference theReference, RequestDetails theRequest, TransactionDetails theTransactionDetails) {
+  public IResourceLookup findTargetResource(@Nonnull RequestPartitionId theRequestPartitionId, String theSourceResourceName, PathAndRef thePathAndRef, RequestDetails theRequest, TransactionDetails theTransactionDetails) {
 
-    if(theType.isAssignableFrom(Bundle.class) || theType.isAssignableFrom(CapabilityStatement.class)) {
-      LOG.info("Skipping findTargetResource() as [{}] is excluded - custom KT2 addition!", theType.getSimpleName());
+    if("Bundle".equals(theSourceResourceName) || "CapabilityStatement".equals(theSourceResourceName)) {
+      LOG.info("Skipping findTargetResource() as [{}] is excluded - custom KT2 addition!", theSourceResourceName);
       return null;
     }
 
-    return super.findTargetResource(theRequestPartitionId, theSearchParam, theSourcePath, theSourceResourceId, theResourceType, theType, theReference, theRequest, theTransactionDetails);
+    return super.findTargetResource(theRequestPartitionId, theSourceResourceName, thePathAndRef, theRequest, theTransactionDetails);
   }
 }
