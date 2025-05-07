61e0fe8b114cb9e7c185ff1cd426a390c62e4743
Joris Scharp <joris@headease.nl>
2025-05-07 08:58:40 +0200

    fix: The getDevice util function returns an optional, however, it would fail when a ResourceNotFoundException or ResourceGoneException was thrown by the dao. Catching these cases and return an Optional.empty.

--- src/main/java/ca/uhn/fhir/jpa/starter/koppeltaal/util/ResourceOriginUtil.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/koppeltaal/util/ResourceOriginUtil.java
@@ -1,60 +1,64 @@
 package ca.uhn.fhir.jpa.starter.koppeltaal.util;
 
 import ca.uhn.fhir.jpa.api.dao.IFhirResourceDao;
-import ca.uhn.fhir.jpa.searchparam.SearchParameterMap;
-import ca.uhn.fhir.rest.api.server.IBundleProvider;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
-import ca.uhn.fhir.rest.param.TokenParam;
+import ca.uhn.fhir.rest.server.exceptions.ResourceGoneException;
+import ca.uhn.fhir.rest.server.exceptions.ResourceNotFoundException;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import java.util.Optional;
 import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.r4.model.*;
 
+import java.util.Optional;
+
 public class ResourceOriginUtil {
 
-	public final static String RESOURCE_ORIGIN_SYSTEM = "http://koppeltaal.nl/fhir/StructureDefinition/resource-origin";
+  public final static String RESOURCE_ORIGIN_SYSTEM = "http://koppeltaal.nl/fhir/StructureDefinition/resource-origin";
 
-	public static Optional<String> getRequesterClientId(RequestDetails requestDetails) {
-		String authorization = requestDetails.getHeader("Authorization");
-		String token = StringUtils.trim(StringUtils.removeStartIgnoreCase(authorization, "Bearer"));
+  public static Optional<String> getRequesterClientId(RequestDetails requestDetails) {
+    String authorization = requestDetails.getHeader("Authorization");
+    String token = StringUtils.trim(StringUtils.removeStartIgnoreCase(authorization, "Bearer"));
 
-		if(StringUtils.isBlank(token)) return Optional.empty();
+    if (StringUtils.isBlank(token)) return Optional.empty();
 
-		// Already validated by the JwtSecurityInterceptor
-		DecodedJWT decode = JWT.decode(token);
+    // Already validated by the JwtSecurityInterceptor
+    DecodedJWT decode = JWT.decode(token);
 
-		final Claim azpClaim = decode.getClaim("azp");
-		String resourceOrigin = azpClaim.asString();
+    final Claim azpClaim = decode.getClaim("azp");
+    String resourceOrigin = azpClaim.asString();
 
-		return StringUtils.isBlank(resourceOrigin) ? Optional.empty() : Optional.of(resourceOrigin);
-	}
+    return StringUtils.isBlank(resourceOrigin) ? Optional.empty() : Optional.of(resourceOrigin);
+  }
 
-	public static Optional<IIdType> getResourceOriginDeviceId(IBaseResource resource) {
-		if(!(resource instanceof DomainResource)) return Optional.empty();
+  public static Optional<IIdType> getResourceOriginDeviceId(IBaseResource resource) {
+    if (!(resource instanceof DomainResource)) return Optional.empty();
 
-		final Extension extension = ((DomainResource) resource).getExtensionByUrl(RESOURCE_ORIGIN_SYSTEM);
+    final Extension extension = ((DomainResource) resource).getExtensionByUrl(RESOURCE_ORIGIN_SYSTEM);
 
-		if(extension == null || extension.isEmpty()) return Optional.empty();
+    if (extension == null || extension.isEmpty()) return Optional.empty();
 
-		final IIdType reference = ((Reference) extension.getValue()).getReferenceElement();
-		return Optional.of(reference);
-	}
+    final IIdType reference = ((Reference) extension.getValue()).getReferenceElement();
+    return Optional.of(reference);
+  }
 
-	public static Optional<Device> getDevice(RequestDetails requestDetails, IFhirResourceDao<Device> deviceDao) {
+  public static Optional<Device> getDevice(RequestDetails requestDetails, IFhirResourceDao<Device> deviceDao) {
 
-		final Optional<String> clientIdOptional = getRequesterClientId(requestDetails);
-		if(clientIdOptional.isEmpty()) return Optional.empty();
+    final Optional<String> clientIdOptional = getRequesterClientId(requestDetails);
+    if (clientIdOptional.isEmpty()) return Optional.empty();
 
-		return getDevice(clientIdOptional.get(), deviceDao, requestDetails);
-	}
+    return getDevice(clientIdOptional.get(), deviceDao, requestDetails);
+  }
 
-	private static Optional<Device> getDevice(String clientId, IFhirResourceDao<Device> deviceDao, RequestDetails requestDetails) {
-    return Optional.of(
-      deviceDao.read(new IdType(clientId), requestDetails)
-    );
-	}
+  private static Optional<Device> getDevice(String clientId, IFhirResourceDao<Device> deviceDao, RequestDetails requestDetails) {
+    try {
+      return Optional.of(
+        deviceDao.read(new IdType(clientId), requestDetails)
+      );
+    } catch (ResourceNotFoundException | ResourceGoneException e) {
+      return Optional.empty();
+    }
+  }
 }
