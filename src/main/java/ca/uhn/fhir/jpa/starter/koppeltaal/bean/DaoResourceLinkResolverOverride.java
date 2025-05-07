8c2b79c5c5d271e7bb337ff3aabd9a6a0f92d383
Roland Groen <roland@headease.nl>
2025-05-07 11:48:13 +0200

    Refactor DaoResourceLinkResolverOverride for enhanced checks
    Refactored `findTargetResource` to improve null safety and enhance readability by using `StringUtils` for string comparisons. Added checks for `ConceptMap` and refined handling of resource types in the referential integrity check exclusion logic. Updated generic type for better flexibility and adherence to type safety.

--- src/main/java/ca/uhn/fhir/jpa/starter/koppeltaal/bean/DaoResourceLinkResolverOverride.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/koppeltaal/bean/DaoResourceLinkResolverOverride.java
@@ -5,13 +5,15 @@ import ca.uhn.fhir.jpa.dao.index.DaoResourceLinkResolver;
 import ca.uhn.fhir.jpa.model.cross.IResourceLookup;
 import ca.uhn.fhir.jpa.searchparam.extractor.PathAndRef;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
+import ca.uhn.fhir.rest.api.server.storage.IResourcePersistentId;
 import ca.uhn.fhir.rest.api.server.storage.TransactionDetails;
+import jakarta.annotation.Nonnull;
+import org.apache.commons.lang3.StringUtils;
+import org.hl7.fhir.instance.model.api.IIdType;
 import org.hl7.fhir.r4.model.Bundle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import javax.annotation.Nonnull;
 
 /**
  * Class that overrides {@link DaoResourceLinkResolver} only to exclude Bundles from the referential integrity check.
@@ -19,24 +21,35 @@ import javax.annotation.Nonnull;
  * HAPI only allows FHIRPaths to be excluded for deletion, not creation.
  */
 @Component
-public class DaoResourceLinkResolverOverride extends DaoResourceLinkResolver {
+public class DaoResourceLinkResolverOverride<T extends IResourcePersistentId<?>> extends DaoResourceLinkResolver<T> {
 
   private static final Logger LOG = LoggerFactory.getLogger(DaoResourceLinkResolverOverride.class);
 
   @Override
-  public IResourceLookup findTargetResource(@Nonnull RequestPartitionId theRequestPartitionId, String theSourceResourceName, PathAndRef thePathAndRef, RequestDetails theRequest, TransactionDetails theTransactionDetails) {
-
+  public IResourceLookup findTargetResource(
+    @Nonnull RequestPartitionId theRequestPartitionId,
+    String theSourceResourceName,
+    PathAndRef thePathAndRef,
+    RequestDetails theRequest,
+    TransactionDetails theTransactionDetails) {
 
     // Skip referential integrity check in `AuditEvent.entity.what` for the Bundle and the CapabilityStatement
-    if("AuditEvent.entity.what".equals(thePathAndRef.getPath()) && thePathAndRef.getRef().getResource() != null) {
-      var type = thePathAndRef.getRef().getResource().fhirType(); //required by the profile
-      if("CapabilityStatement".equals(type) || "Bundle".equals(type)) {
+    String sourcePath = thePathAndRef.getPath();
+    IIdType referenceElement = thePathAndRef.getRef().getReferenceElement();
+    if (StringUtils.equals("AuditEvent.entity.what", sourcePath)) {
+      String type = "";
+      if (referenceElement.getResourceType() != null) {
+        type = thePathAndRef.getRef().getReferenceElement().getResourceType(); //required by the profile
+      } else if (thePathAndRef.getRef().getResource() != null) {
+        type = thePathAndRef.getRef().getResource().fhirType();
+      }
+      if (StringUtils.equals("CapabilityStatement", type) || StringUtils.equals("Bundle", type)) {
         LOG.info("Skipping findTargetResource() as [{}] is excluded - custom KT2 addition!", type);
         return null;
       }
     }
 
-    if("Bundle".equals(theSourceResourceName) || "CapabilityStatement".equals(theSourceResourceName)) {
+    if (StringUtils.equals("Bundle", theSourceResourceName) || StringUtils.equals("CapabilityStatement", theSourceResourceName) || StringUtils.equals("ConceptMap", theSourceResourceName)) {
       LOG.info("Skipping findTargetResource() as [{}] is excluded - custom KT2 addition!", theSourceResourceName);
       return null;
     }
